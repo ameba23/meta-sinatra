fecd912efc3747ba7395384ac2d83d93dc8c5e53
ameba23 <ameba23@systemli.org>
2017-10-10 13:13:34 +0200

    got it spitting out the whole db in a very untidy way

--- test.rb
+++ test.rb
@@ -6,15 +6,15 @@ DB = 'http://localhost:5984/metadb'
 
 get '/' do
   data = RestClient.get "#{DB}/_all_docs"
-  result = JSON.parse(data)
-  result['rows'].each do |item|    
-    data2 = RestClient.get "#{DB}/#{item['id']}"
-    result1 = JSON.parse(data2)
-    if result1['_id'].include? "_design"
-    else 
-      @result2 = result1
-    end
+  @result = JSON.parse(data)
+  #result['rows'].each do |item|    
+  #  data2 = RestClient.get "#{DB}/#{item['id']}"
+    #result1 = JSON.parse(data2)
+    #if result1['_id'].include? "_design"
+    #else 
+    #  @result2 = result1
+    #end
     #@result2 = item['id']
-  end
+  #end
   erb :index
 end
