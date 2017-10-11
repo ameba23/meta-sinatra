7ca4451447af87b623a0fa1eb5ff1bbaeb3488b8
ameba23 <ameba23@systemli.org>
2017-10-11 12:22:18 +0200

    added totals for filetypes, tidied up

--- test.rb
+++ test.rb
@@ -1,8 +1,43 @@
 require 'sinatra'
 require 'json'
 require 'rest_client'
+require 'thin'
+
+# todo:
+#   use hash as _id
+#   view x recently added files on front page -need to add timestamp?
+#   wirte view to calculate approximate sizes
+#   switch to slim beofre doing too much more html
+#   add page for each file
 
 DB = 'http://localhost:5984/metadb'
+
+def numberfiles
+  data2 = RestClient.get "#{DB}/_design/tryit/_view/files"
+  data3 = JSON.parse(data2)
+  return data3['total_rows']
+end
+
+def numberaudio
+  data3 = JSON.parse(RestClient.get "#{DB}/_design/tryit/_view/audio")
+  return data3['total_rows']
+end
+
+def numberimages
+  data3 = JSON.parse(RestClient.get "#{DB}/_design/tryit/_view/images")
+  return data3['total_rows']
+end
+
+def somefiles
+  numfiles = 10
+  data3 = JSON.parse(RestClient.get "#{DB}/_design/tryit/_view/files")
+  # *** first check there are at least x files
+  if data3['rows'].length > numfiles
+    return data3['rows'][0..numfiles]
+  else 
+    return data3['rows'][0..data3['rows'].length]
+  end
+end
 
 get '/' do
   data = RestClient.get "#{DB}/_all_docs"
@@ -17,4 +52,11 @@ get '/' do
     #@result2 = item['id']
   #end
   erb :index
+end
+
+get '/audio' do
+  "should list first x audio files"
+end
+get '/images' do
+  "should list first x image files"
 end
