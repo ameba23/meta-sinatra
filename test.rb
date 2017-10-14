440947d232fcfdce5b82dac988163ff5982da65e
ameba23 <ameba23@systemli.org>
2017-10-14 14:13:27 +0200

    many improvements and cleaned up code

--- test.rb
+++ test.rb
@@ -2,61 +2,51 @@ require 'sinatra'
 require 'json'
 require 'rest_client'
 require 'thin'
+require 'slim'
 
 # todo:
 #   use hash as _id
 #   view x recently added files on front page -need to add timestamp?
 #   wirte view to calculate approximate sizes
-#   switch to slim beofre doing too much more html
-#   add page for each file
+#   read db name from config file?
+#   use couchrest or somethign similar?
+#   switch to rails?
 
 DB = 'http://localhost:5984/metadb'
 
-def numberfiles
-  data2 = RestClient.get "#{DB}/_design/tryit/_view/files"
-  data3 = JSON.parse(data2)
-  return data3['total_rows']
+
+def numberof(filetype)
+  data = JSON.parse(RestClient.get "#{DB}/_design/tryit/_view/#{filetype}")
+  return data['total_rows']
 end
 
-def numberaudio
-  data3 = JSON.parse(RestClient.get "#{DB}/_design/tryit/_view/audio")
-  return data3['total_rows']
-end
-
-def numberimages
-  data3 = JSON.parse(RestClient.get "#{DB}/_design/tryit/_view/images")
-  return data3['total_rows']
-end
-
-def somefiles
-  numfiles = 10
-  data3 = JSON.parse(RestClient.get "#{DB}/_design/tryit/_view/files")
+def somefiles(filetype)
+  numfiles = 50
+  data = JSON.parse(RestClient.get "#{DB}/_design/tryit/_view/#{filetype}")
   # *** first check there are at least x files
-  if data3['rows'].length > numfiles
-    return data3['rows'][0..numfiles]
+  if data['rows'].length > numfiles
+    return data['rows'][0..numfiles]
   else 
-    return data3['rows'][0..data3['rows'].length]
+    return data['rows'][0..data['rows'].length]
   end
 end
 
 get '/' do
-  data = RestClient.get "#{DB}/_all_docs"
-  @result = JSON.parse(data)
-  #result['rows'].each do |item|    
-  #  data2 = RestClient.get "#{DB}/#{item['id']}"
-    #result1 = JSON.parse(data2)
-    #if result1['_id'].include? "_design"
-    #else 
-    #  @result2 = result1
-    #end
-    #@result2 = item['id']
-  #end
-  erb :index
+  @page_title = ''
+  # @result = JSON.parse(RestClient.get "#{DB}/_all_docs")
+  slim :index
 end
 
-get '/audio' do
-  "should list first x audio files"
+# Pages listing files by category (eg: audio)
+get '/filetype/:filetype' do
+  @page_title = params[:filetype]
+  @filetype = params[:filetype]
+  slim :bytype
 end
-get '/images' do
-  "should list first x image files"
+
+# Pages for individual files:
+get '/files/:hash' do
+  @page_title = params[:hash]
+  @hash = JSON.parse(RestClient.get "#{DB}/#{params[:hash]}")
+  slim :hash
 end
